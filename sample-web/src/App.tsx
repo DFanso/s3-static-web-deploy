1acaa8426e23d858c774a01902eba465b4d804a7
Leo Gavin <leogavin123@outlook.com>
2024-05-23 19:37:33 +0530

    Update App.tsx

--- sample-web/src/App.tsx
+++ sample-web/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hosted on AWS s3</h1>
+      <h1>CDN By AWS CloudFront</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
